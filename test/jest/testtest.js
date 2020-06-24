b310c00461c23293fe87e51a134c22906fdd23af
fadikamohamed <fadika.mohamed02@gmail.com>
2020-06-24 14:00:53 +0200

    feat jest test

---
+++ test/jest/testtest.js
@@ -0,0 +1,22 @@
+'use strict';
+jest.unmock('../src/App.tsx');
+
+import React from 'react';
+import {
+  renderIntoDocument,
+  findRenderedDOMComponentWithTag
+} from 'react-addons-test-utils';
+import Product from '../src/App.tsx';
+
+describe('Product', () => {
+  it('Should render product with name, price and stock', () => {
+    const testedComponent = renderIntoDocument(
+      <Product
+        product={{}}
+        withStock={false}
+      />
+    );
+    const productTitle = findRenderedDOMComponentWithTag(testedComponent, 'li');
+    expect(productTitle.textContent).toEqual('');
+  });
+});
